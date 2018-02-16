447f7b5d0aefaca77740859aa0becc171ef151ff
Роман Чебаненко <roman_chebanenko@MacBook-Air.local>
2018-02-16 18:12:01 +0300

    added 6.3.py

---
+++ exercises/6/6.3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from sys import argv
+import re
+
+# Очистка таблицы с MAC-адресами от ненужного и потом поиск по VLAN
+# Обработка производится построчно
+
