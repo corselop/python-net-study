f9a00c31298c468d0dda6c824dba2338a67114fd
Роман Чебаненко <roman_chebanenko@MacBook-Air.local>
2018-03-14 15:54:34 +0300

    7.1 initial done

---
+++ exercises/7/7.1.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from sys import argv
+from operator import itemgetter
+import re
+
+# фукнция, которая генерирует конфигурацию для access-портов
+
+# эта функция получает на вход словарь и возвращает конфигурацию на основе шаблона
+
+# это словарь
+access_dict = { 'FastEthernet0/12':10,
+                'FastEthernet0/14':11,
+                'FastEthernet0/16':17,
+                'FastEthernet0/17':150 }
+
+# функция
+
+def generate_access_config(access):
+    """
+    access - словарь access-портов,
+    для которых необходимо сгенерировать конфигурацию, вида:
+        { 'FastEthernet0/12':10,
+          'FastEthernet0/14':11,
+          'FastEthernet0/16':17}
+
+    Возвращает список всех портов в режиме access с конфигурацией на основе шаблона
+    """
+    access_template = ['switchport mode access',
+                       'switchport access vlan',
+                       'switchport nonegotiate',
+                       'spanning-tree portfast',
+                       'spanning-tree bpduguard enable']
+
+    config=[]
+
+    for interface in access:
+        config.append('interface ' + interface)
+#        print access[interface]
+        for item in access_template:
+            if re.search('vlan',item):
+                config.append(item + '' + str(access[interface]))
+            else:
+                config.append(item)
+
+    print config
+
+generate_access_config(access_dict)
